0dc9e3aa549a4730d63df0420cd381f2f6157137
banan <banan.faouri2001@gmail.com>
2023-12-27 01:57:18 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+var customerName ='bob';
+const leastFavoriteCustomer = 'apple'
+function upperCaseCustomerName(){
+   return customerName = customerName.toUpperCase()
+   
+  
+}
+upperCaseCustomerName();
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer(){
+    bestCustomer='maybe bob'
+    return bestCustomer
+}
+
+function changeLeastFavoriteCustomer(){
+   return (leastFavoriteCustomer='not apple')
+}
